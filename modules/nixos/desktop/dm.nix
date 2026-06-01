33911f92481db281458f69bd3600230a109f014a
Ezozbek <git@ezozbek.dev>
2026-06-01 16:05:06 +0500

    update greeter conf

--- modules/nixos/desktop/dm.nix
+++ modules/nixos/desktop/dm.nix
@@ -5,6 +5,7 @@
 }: {
   services.greetd = {
     enable = true;
+    useTextGreeter = true;
     settings = {
       default_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions:${pkgs.hyprland}/share/wayland-sessions --remember --remember-user-session";
