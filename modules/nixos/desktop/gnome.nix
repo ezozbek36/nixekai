7689e0c1ac25372dda09c4087bb79030cba2fc30
Ezozbek <git@ezozbek.dev>
2026-08-01 17:31:34 +0500

    ayato: use latest mission center
    Signed-off-by: Ezozbek <git@ezozbek.dev>

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -1,4 +1,16 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
+  nixpkgs.overlays = lib.singleton (
+    final: prev: {
+      mission-center =
+        final.fetchurl {
+          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/dc4be957075ee6a0e61465291cf36df49c565ff1/pkgs/by-name/mi/mission-center/package.nix";
+          hash = "sha256-WB8QqUnc/wDwxhFRxoGz1tw2qzMdX4LJfeErQBPipnk=";
+        }
+        |> (x: x.outPath)
+        |> lib.flip final.callPackage {};
+    }
+  );
+
   services.desktopManager.gnome = {
     enable = true;
     extraGSettingsOverrides = ''
